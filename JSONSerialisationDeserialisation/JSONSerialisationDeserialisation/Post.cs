00dc59dd55edb952331d5bca6960b879d084aaa5
circamanuel <163194187+circamanuel@users.noreply.github.com>
2025-11-24 14:36:13 +0100

    JSON API Deserialisation

---
+++ JSONSerialisationDeserialisation/JSONSerialisationDeserialisation/Post.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace JSONSerialisationDeserialisation
+{
+    public class Post
+    {
+        // Holt aus Json id und setzt sie als Id
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+     }
+}
---
+++ JSONSerialisationDeserialisation/JSONSerialisationDeserialisation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace JSONSerialisationDeserialisation
+{
+    internal class Program
+    {
+        static  async Task Main(string[] args)
+        {
+
+            string url = "https://my-json-server.typicode.com/typicode/demo/posts";
+            // an http client to send the get request
+            HttpClient httpClient = new HttpClient();
+
+            try
+            {
+                var httpResponseMessage = await httpClient.GetAsync(url);
+                //read the string  from the response's content
+                string jsonResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                Console.WriteLine(jsonResponse);
+
+                // Deserialize the json response into a C# array of type Post[]
+                var myPost = JsonConvert.DeserializeObject<Post[]>(jsonResponse);
+
+                //print the array objects using a foreach loop
+                foreach (var post in myPost)
+                {
+                    Console.WriteLine($"{ post.Id} { post.Title}");
+                }
+            }
+            catch (Exception e)
+            {
+                //print the exception message
+                Console.WriteLine(e.Message);
+            }
+            finally
+            { 
+                httpClient.Dispose();
+            }
+        }
+    }
+}
